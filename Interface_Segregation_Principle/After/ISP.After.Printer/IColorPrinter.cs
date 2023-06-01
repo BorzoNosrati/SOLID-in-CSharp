ab1d886fa659303ec682910663aa7dde71f030dd
Borzo Nosrati <borzo.nosrati@outlook.com>
2023-06-01 20:16:23 +0330

    ISP

--- Interface_Segregation_Principle/After/ISP.After.Printer/IColorPrinter.cs
+++ Interface_Segregation_Principle/After/ISP.After.Printer/IColorPrinter.cs
@@ -2,5 +2,5 @@
 
 public interface IColorPrinter
 {
-    void Print();
+    void ColorPrint();
 }
--- Interface_Segregation_Principle/After/ISP.After.Printer/PrinterA.cs
+++ Interface_Segregation_Principle/After/ISP.After.Printer/PrinterA.cs
@@ -3,6 +3,6 @@
 public class PrinterA : IPrinter, IColorPrinter, IScanPrinter
 {
     public void Print() => Console.WriteLine("Print Done.");
-    void IColorPrinter.Print() => Console.WriteLine("Color Print Done.");
+    public void ColorPrint() => Console.WriteLine("Color Print Done.");
     public void Scan() => Console.WriteLine("Scan Done.");
 }
